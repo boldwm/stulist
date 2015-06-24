9acebe4813d551775c3284c32019915febe2e8fb
857635086 <894587632@qq.com>
2015-06-24 21:02:10 +0800

    后台

---
+++ handlers.py
@@ -0,0 +1,30 @@
+﻿# -*- coding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+
+from dbconn import db_cursor
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("pages/main.html")
+
+
+class CourseListHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("pages/cou_list.html", courses = dal_list_courses())
+
+
+def dal_list_courses():
+    data = []
+    with db_cursor() as cur : # 取得操作数据的游标，记为cur
+        s = """
+        SELECT cou_sn, stu_no, cou_no, name, sex,grade FROM course ORDER BY cou_sn DESC
+        """
+        cur.execute(s)      
+        for r in cur.fetchall():
+            cou = dict(cou_sn=r[0], stu_no=r[1], cou_no=r[2], name=r[3], sex=r[4],grade=r[5])
+            data.append(cou)
+    print(data)
+    return data
+
